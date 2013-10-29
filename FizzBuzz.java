dd2b9e11a525bca0ffc65ce9ecf955770daba813
Mitch Worsey <MitchWorsey@guest-wireless-upc-nat-206-117-88-010.usc.edu>
2013-10-29 14:56:39 -0700

    initial code for FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,11 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
+public class FizzBuzz{
+  public static void main(String[] args){
+    for(int i = 0; i < 100; i++){
+      System.out.println(i);
+    }
+  }
+}
